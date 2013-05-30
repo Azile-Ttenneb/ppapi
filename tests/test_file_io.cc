Azile-Ttenneb/ppapi

commit d69747db14b7ff8464ef4ea4a9cf25a9af12c46a

NaCl: Let SerializedHandle carry open flag.
The (deprecated) NaClDescIoDescMakeFromHandle used to assume the
given file handle was opened read-write, which breaks mmap on
Windows when the file is opened read-only.

In this change, FileIO::RequestOSFileHandle now passes the actual
open flag to the new NaClDescIoDescFromHandleAllocCtor.

BUG=183015

Review URL: https://chromiumcodereview.appspot.com/14836003

git-svn-id: http://src.chromium.org/svn/trunk/src/ppapi@200088 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

tests/test_file_io.cc
@@ -126,6 +126,21 @@ int32_t ReadToArrayEntireFile(PP_Instance instance,
   return PP_OK;
 }
 
+bool ReadEntireFileFromFileHandle(int fd, std::string* data) {
+  if (lseek(fd, 0, SEEK_SET) < 0)
+    return false;
+  data->clear();
+
+  int ret;
+  do {
+    char buf[8192];
+    ret = read(fd, buf, sizeof(buf));
+    if (ret > 0)
+      data->append(buf, ret);
+  } while (ret > 0);
+  return ret == 0;
+}
+
 int32_t WriteEntireBuffer(PP_Instance instance,
                           pp::FileIO* file_io,
                           int32_t offset,
@@ -169,6 +184,7 @@ void TestFileIO::RunTests(const std::string& filter) {
   RUN_CALLBACK_TEST(TestFileIO, NotAllowMixedReadWrite, filter);
   RUN_CALLBACK_TEST(TestFileIO, WillWriteWillSetLength, filter);
   RUN_CALLBACK_TEST(TestFileIO, RequestOSFileHandle, filter);
+  RUN_CALLBACK_TEST(TestFileIO, Mmap, filter);
 
   // TODO(viettrungluu): add tests:
   //  - that PP_ERROR_PENDING is correctly returned
@@ -1089,45 +1105,172 @@ std::string TestFileIO::TestRequestOSFileHandle() {
     return ReportError("read for native FD count mismatch", cnt);
   if (msg != buf)
     return ReportMismatch("read for native FD", buf, msg);
+  PASS();
+}
 
-  // TODO(hamaji): Test CreateFileMapping for windows.
+std::string TestFileIO::TestMmap() {
 #if !defined(PPAPI_OS_WIN)
+  TestCompletionCallback callback(instance_->pp_instance(), callback_type());
+
+  pp::FileSystem file_system(instance_, PP_FILESYSTEMTYPE_LOCALTEMPORARY);
+  pp::FileRef file_ref(file_system, "/file_os_fd");
+
+  callback.WaitForResult(file_system.Open(1024, callback.GetCallback()));
+  ASSERT_EQ(PP_OK, callback.result());
+
+  pp::FileIO_Private file_io(instance_);
+  callback.WaitForResult(file_io.Open(file_ref,
+                                      PP_FILEOPENFLAG_CREATE |
+                                      PP_FILEOPENFLAG_TRUNCATE |
+                                      PP_FILEOPENFLAG_READ |
+                                      PP_FILEOPENFLAG_WRITE,
+                                      callback.GetCallback()));
+  ASSERT_EQ(PP_OK, callback.result());
+
+  TestCompletionCallbackWithOutput<pp::PassFileHandle> output_callback(
+      instance_->pp_instance(), callback_type());
+  output_callback.WaitForResult(
+      file_io.RequestOSFileHandle(output_callback.GetCallback()));
+  PP_FileHandle handle = output_callback.output().Release();
+  ASSERT_EQ(PP_OK, output_callback.result());
+
+  if (handle == PP_kInvalidFileHandle)
+    return "FileIO::RequestOSFileHandle() returned a bad file handle.";
+  int fd = handle;
+  if (fd < 0)
+    return "FileIO::RequestOSFileHandle() returned a bad file descriptor.";
+
+  // Check write(2) for the native FD.
+  const std::string msg = "foobar";
+  ssize_t cnt = write(fd, msg.data(), msg.size());
+  if (cnt < 0)
+    return ReportError("write for native FD returned error", errno);
+  if (cnt != static_cast<ssize_t>(msg.size()))
+    return ReportError("write for native FD count mismatch", cnt);
+
+  // BEGIN mmap(2) test with a file handle opened in READ-WRITE mode.
   // Check mmap(2) for read.
-  char* mapped = reinterpret_cast<char*>(
-      mmap(NULL, msg.size(), PROT_READ, MAP_PRIVATE, fd, 0));
-  if (mapped == MAP_FAILED)
-    return ReportError("mmap(r) for native FD returned errno", errno);
-  // Make sure the buffer is cleared.
-  buf = std::string(msg.size(), '\0');
-  memcpy(&buf[0], mapped, msg.size());
-  if (msg != buf)
-    return ReportMismatch("mmap(r) for native FD", buf, msg);
-  int r = munmap(mapped, msg.size());
-  if (r < 0)
-    return ReportError("munmap for native FD returned error", errno);
+  {
+    char* mapped = reinterpret_cast<char*>(
+        mmap(NULL, msg.size(), PROT_READ, MAP_PRIVATE, fd, 0));
+    if (mapped == MAP_FAILED)
+      return ReportError("mmap(r) for native FD returned errno", errno);
+    // Make sure the buffer is cleared.
+    std::string buf = std::string(msg.size(), '\0');
+    memcpy(&buf[0], mapped, msg.size());
+    if (msg != buf)
+      return ReportMismatch("mmap(r) for native FD", buf, msg);
+    int r = munmap(mapped, msg.size());
+    if (r < 0)
+      return ReportError("munmap for native FD returned error", errno);
+  }
 
-  // Check mmap(2) for write.
-  mapped = reinterpret_cast<char*>(
-      mmap(NULL, msg.size(), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0));
-  if (mapped == MAP_FAILED)
-    return ReportError("mmap(w) for native FD returned errno", errno);
-  // s/foo/baz/
-  strcpy(mapped, "baz");
+  // Check mmap(2) for write with MAP_PRIVATE
+  {
+    char* mapped = reinterpret_cast<char*>(
+        mmap(NULL, msg.size(), PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0));
+    if (mapped == MAP_FAILED)
+      return ReportError("mmap(r) for native FD returned errno", errno);
+    // Make sure the file is not polluted by writing to privage mmap.
+    strncpy(mapped, "baz", 3);
+    std::string read_buffer;
+    ASSERT_TRUE(ReadEntireFileFromFileHandle(fd, &read_buffer));
+    if (msg != read_buffer)
+      return ReportMismatch("file content != msg", read_buffer, msg);
+    int r = munmap(mapped, msg.size());
+    if (r < 0)
+      return ReportError("munmap for native FD returned error", errno);
+  }
 
-  r = munmap(mapped, msg.size());
-  if (r < 0)
-    return ReportError("munmap for native FD returned error", errno);
-#endif
+  // Check mmap(2) for write with MAP_SHARED.
+  {
+    char* mapped = reinterpret_cast<char*>(
+        mmap(NULL, msg.size(), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0));
+    if (mapped == MAP_FAILED)
+      return ReportError("mmap(w) for native FD returned errno", errno);
+    // s/foo/baz/
+    strncpy(mapped, "baz", 3);
+    std::string read_buffer;
+    ASSERT_TRUE(ReadEntireFileFromFileHandle(fd, &read_buffer));
+    if (read_buffer != "bazbar")
+      return ReportMismatch("file content != msg", read_buffer, "bazbar");
+    int r = munmap(mapped, msg.size());
+    if (r < 0)
+      return ReportError("munmap for native FD returned error", errno);
+  }
+  // END mmap(2) test with a file handle opened in READ-WRITE mode.
 
-#if defined(PPAPI_OS_WIN)
-  int r = _close(fd);
-#else
-  r = close(handle);
-#endif
-  if (r < 0)
+  if (close(fd) < 0)
     return ReportError("close for native FD returned error", errno);
 
-  // TODO(hamaji): Check if the file is actually updated?
+  // BEGIN mmap(2) test with a file handle opened in READONLY mode.
+  file_io = pp::FileIO_Private(instance_);
+  callback.WaitForResult(file_io.Open(file_ref,
+                                      PP_FILEOPENFLAG_READ,
+                                      callback.GetCallback()));
+  ASSERT_EQ(PP_OK, callback.result());
+
+  output_callback = TestCompletionCallbackWithOutput<pp::PassFileHandle>(
+      instance_->pp_instance(), callback_type());
+  output_callback.WaitForResult(
+      file_io.RequestOSFileHandle(output_callback.GetCallback()));
+  handle = output_callback.output().Release();
+  ASSERT_EQ(PP_OK, output_callback.result());
+
+  if (handle == PP_kInvalidFileHandle)
+    return "FileIO::RequestOSFileHandle() returned a bad file handle.";
+  fd = handle;
+  if (fd < 0)
+    return "FileIO::RequestOSFileHandle() returned a bad file descriptor.";
+
+  const std::string msg2 = "bazbar";
+
+  // Check mmap(2) for read.
+  {
+    char* mapped = reinterpret_cast<char*>(
+        mmap(NULL, msg2.size(), PROT_READ, MAP_PRIVATE, fd, 0));
+    if (mapped == MAP_FAILED)
+      return ReportError("mmap(r) for native FD returned errno", errno);
+    // Make sure the buffer is cleared.
+    std::string buf = std::string(msg2.size(), '\0');
+    memcpy(&buf[0], mapped, msg2.size());
+    if (msg2 != buf)
+      return ReportMismatch("mmap(r) for native FD", buf, msg2);
+    int r = munmap(mapped, msg2.size());
+    if (r < 0)
+      return ReportError("munmap for native FD returned error", errno);
+  }
+
+  // Check mmap(2) for write with MAP_PRIVATE
+  {
+    char* mapped = reinterpret_cast<char*>(
+        mmap(NULL, msg2.size(), PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0));
+    if (mapped == MAP_FAILED)
+      return ReportError("mmap(r) for native FD returned errno", errno);
+    // Make sure the file is not polluted by writing to privage mmap.
+    strncpy(mapped, "baz", 3);
+    std::string read_buffer;
+    ASSERT_TRUE(ReadEntireFileFromFileHandle(fd, &read_buffer));
+    if (msg2 != read_buffer)
+      return ReportMismatch("file content != msg2", read_buffer, msg2);
+    int r = munmap(mapped, msg2.size());
+    if (r < 0)
+      return ReportError("munmap for native FD returned error", errno);
+  }
+
+  // Check mmap(2) for write with MAP_SHARED.
+  {
+    char* mapped = reinterpret_cast<char*>(
+        mmap(NULL, msg2.size(), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0));
+    if (mapped != MAP_FAILED)
+      return ReportError("mmap(w) for native FD must fail when opened readonly",
+                         -1);
+  }
+  // END mmap(2) test with a file handle opened in READONLY mode.
+
+  if (close(fd) < 0)
+    return ReportError("close for native FD returned error", errno);
+#endif  // !defined(PPAPI_OS_WIN)
 
   PASS();
 }

tests/test_file_io.h
@@ -48,6 +48,7 @@ class TestFileIO : public TestCase {
   std::string TestNotAllowMixedReadWrite();
   std::string TestWillWriteWillSetLength();
   std::string TestRequestOSFileHandle();
+  std::string TestMmap();
 
   // Helper method used by TestOpen().
   // |expectations| is a combination of OpenExpectation values. The followings

proxy/ppapi_param_traits.cc
@@ -374,7 +374,7 @@ bool ParamTraits<ppapi::proxy::SerializedHandle>::Read(const Message* m,
     case ppapi::proxy::SerializedHandle::FILE: {
       IPC::PlatformFileForTransit desc;
       if (ParamTraits<IPC::PlatformFileForTransit>::Read(m, iter, &desc)) {
-        r->set_file_handle(desc);
+        r->set_file_handle(desc, header.open_flag);
         return true;
       }
       break;

proxy/serialized_handle.cc
@@ -97,12 +97,16 @@ bool SerializedHandle::WriteHeader(const Header& hdr, Pickle* pickle) {
     if (!pickle->WriteUInt32(hdr.size))
       return false;
   }
+  if (hdr.type == FILE) {
+    if (!pickle->WriteInt(hdr.open_flag))
+      return false;
+  }
   return true;
 }
 
 // static
 bool SerializedHandle::ReadHeader(PickleIterator* iter, Header* hdr) {
-  *hdr = Header(INVALID, 0);
+  *hdr = Header(INVALID, 0, 0);
   int type = 0;
   if (!iter->ReadInt(&type))
     return false;
@@ -116,9 +120,15 @@ bool SerializedHandle::ReadHeader(PickleIterator* iter, Header* hdr) {
       valid_type = true;
       break;
     }
+    case FILE: {
+      int open_flag = 0;
+      if (!iter->ReadInt(&open_flag))
+        return false;
+      hdr->open_flag = open_flag;
+      valid_type = true;
+    }
     case SOCKET:
     case CHANNEL_HANDLE:
-    case FILE:
     case INVALID:
       valid_type = true;
       break;

proxy/serialized_handle.h
@@ -28,12 +28,13 @@ class PPAPI_PROXY_EXPORT SerializedHandle {
  public:
   enum Type { INVALID, SHARED_MEMORY, SOCKET, CHANNEL_HANDLE, FILE };
   struct Header {
-    Header() : type(INVALID), size(0) {}
-    Header(Type type_arg, uint32 size_arg)
-        : type(type_arg), size(size_arg) {
+    Header() : type(INVALID), size(0), open_flag(0) {}
+    Header(Type type_arg, uint32 size_arg, int32 open_flag_arg)
+        : type(type_arg), size(size_arg), open_flag(open_flag_arg) {
     }
     Type type;
     uint32 size;
+    int32 open_flag;
   };
 
   SerializedHandle();
@@ -64,6 +65,9 @@ class PPAPI_PROXY_EXPORT SerializedHandle {
     DCHECK(is_socket() || is_channel_handle() || is_file());
     return descriptor_;
   }
+  int32 open_flag() const {
+    return open_flag_;
+  }
   void set_shmem(const base::SharedMemoryHandle& handle, uint32 size) {
     type_ = SHARED_MEMORY;
     shm_handle_ = handle;
@@ -85,12 +89,14 @@ class PPAPI_PROXY_EXPORT SerializedHandle {
     shm_handle_ = base::SharedMemory::NULLHandle();
     size_ = 0;
   }
-  void set_file_handle(const IPC::PlatformFileForTransit& descriptor) {
+  void set_file_handle(const IPC::PlatformFileForTransit& descriptor,
+                       int32 open_flag) {
     type_ = FILE;
 
     descriptor_ = descriptor;
     shm_handle_ = base::SharedMemory::NULLHandle();
     size_ = 0;
+    open_flag_ = open_flag;
   }
   void set_null_shmem() {
     set_shmem(base::SharedMemory::NULLHandle(), 0);
@@ -102,12 +108,12 @@ class PPAPI_PROXY_EXPORT SerializedHandle {
     set_channel_handle(IPC::InvalidPlatformFileForTransit());
   }
   void set_null_file_handle() {
-    set_file_handle(IPC::InvalidPlatformFileForTransit());
+    set_file_handle(IPC::InvalidPlatformFileForTransit(), 0);
   }
   bool IsHandleValid() const;
 
   Header header() const {
-    return Header(type_, size_);
+    return Header(type_, size_, open_flag_);
   }
 
   // Closes the handle and sets it to invalid.
@@ -131,8 +137,11 @@ class PPAPI_PROXY_EXPORT SerializedHandle {
   base::SharedMemoryHandle shm_handle_;
   uint32 size_;
 
-  // This is valid if type == SOCKET || type == CHANNEL_HANDLE.
+  // This is valid if type == SOCKET || type == CHANNEL_HANDLE || type == FILE.
   IPC::PlatformFileForTransit descriptor_;
+
+  // This is valid if type == FILE.
+  int32 open_flag_;
 };
 
 }  // namespace proxy

commit 5c794e1f3dc7afe9193efa2c7d12876ad8a468ea

Add a PPAPI browser tests for an issue of NaCl crash.
These tests verifie that the crash of NaCl module introduced by r200088 does
not occur when PPB_FileIO_Private::RequestOSFileHandle is called with
PPB_FileIO opened with PP_FILEOPENFLAG_EXCLUSIVE. 

BUG=243241,241726
TEST=
browser_tests --gtest_filter="PPAPI*Test.FileIO" passes
Revert r200088
browser_tests --gtest_filter="PPAPI*Test.FileIO" crashes

Review URL: https://chromiumcodereview.appspot.com/15864005

git-svn-id: http://src.chromium.org/svn/trunk/src/ppapi@203124 4ff67af0-8c30-449e-8e8b-ad334ec8d88c

tests/test_file_io.cc
@@ -184,6 +184,7 @@ void TestFileIO::RunTests(const std::string& filter) {
   RUN_CALLBACK_TEST(TestFileIO, NotAllowMixedReadWrite, filter);
   RUN_CALLBACK_TEST(TestFileIO, WillWriteWillSetLength, filter);
   RUN_CALLBACK_TEST(TestFileIO, RequestOSFileHandle, filter);
+  RUN_CALLBACK_TEST(TestFileIO, RequestOSFileHandleWithOpenExclusive, filter);
   RUN_CALLBACK_TEST(TestFileIO, Mmap, filter);
 
   // TODO(viettrungluu): add tests:
@@ -1105,6 +1106,39 @@ std::string TestFileIO::TestRequestOSFileHandle() {
     return ReportError("read for native FD count mismatch", cnt);
   if (msg != buf)
     return ReportMismatch("read for native FD", buf, msg);
+  PASS();
+}
+
+// Calling RequestOSFileHandle with the FileIO that is opened with
+// PP_FILEOPENFLAG_EXCLUSIVE used to cause NaCl module to crash while loading.
+// This is a regression test for crbug.com/243241.
+std::string TestFileIO::TestRequestOSFileHandleWithOpenExclusive() {
+  TestCompletionCallback callback(instance_->pp_instance(), callback_type());
+
+  pp::FileSystem file_system(instance_, PP_FILESYSTEMTYPE_LOCALTEMPORARY);
+  pp::FileRef file_ref(file_system, "/file_os_fd2");
+
+  callback.WaitForResult(file_system.Open(1024, callback.GetCallback()));
+  ASSERT_EQ(PP_OK, callback.result());
+
+  pp::FileIO_Private file_io(instance_);
+  callback.WaitForResult(file_io.Open(file_ref,
+                                      PP_FILEOPENFLAG_CREATE |
+                                      PP_FILEOPENFLAG_READ |
+                                      PP_FILEOPENFLAG_WRITE |
+                                      PP_FILEOPENFLAG_EXCLUSIVE,
+                                      callback.GetCallback()));
+  ASSERT_EQ(PP_OK, callback.result());
+
+  TestCompletionCallbackWithOutput<pp::PassFileHandle> output_callback(
+      instance_->pp_instance(), callback_type());
+  output_callback.WaitForResult(
+      file_io.RequestOSFileHandle(output_callback.GetCallback()));
+  PP_FileHandle handle = output_callback.output().Release();
+  if (handle == PP_kInvalidFileHandle)
+    return "FileIO::RequestOSFileHandle() returned a bad file handle.";
+  ASSERT_EQ(PP_OK, output_callback.result());
+
   PASS();
 }
 

tests/test_file_io.h
@@ -48,6 +48,7 @@ class TestFileIO : public TestCase {
   std::string TestNotAllowMixedReadWrite();
   std::string TestWillWriteWillSetLength();
   std::string TestRequestOSFileHandle();
+  std::string TestRequestOSFileHandleWithOpenExclusive();
   std::string TestMmap();
 
   // Helper method used by TestOpen().